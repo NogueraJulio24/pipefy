ecd52822afa3f57a7c5affd22786ca6da1580df0
Julio Noguera <noguerajulio24@gmail.com>
2019-10-13 21:51:21 -0500

    adding styles

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+        <header>
+            <Label color="#7159c1" />
+        </header>
+        <p>Hacer migración</p>
+        <img src="https://api.adorable.io/avatars/259/abott@adorable.png4" alt="" />
+    </Container>
+  );
+}
