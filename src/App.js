16c288077cc7687b1c990c8d6894cced53b6c85a
Julio Noguera <noguerajulio24@gmail.com>
2019-10-13 21:10:23 -0500

    add Material Design

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <Header/>
+      <Board />
+
+      <GlobalStyle />
     </>
   );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAdd } from 'react-icons/md';
+import { Container } from './styles';
 
 export default function List() {
   return (
-    <div />
+    <Container>
+      <header>
+        <h2>Tareas</h2>
+        <button type="button">
+          <MdAdd size={24} color="#FFF" />
+        </button>
+      </header>
+    </Container>
   );
 }
